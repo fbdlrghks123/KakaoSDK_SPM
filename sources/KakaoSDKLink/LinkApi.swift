b90479b2699d86a5e7f2f018fd21e758f7e7e191
ickhwan Ryu <fbdlrghks123@naver.com>
2021-03-25 13:31:48 +0900

    KakaoLink import UIKit 누락 수정

--- sources/KakaoSDKLink/LinkApi.swift
+++ sources/KakaoSDKLink/LinkApi.swift
@@ -12,8 +12,8 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
+import UIKit
 import Foundation
-
 import Alamofire
 import KakaoSDKCommon
 import KakaoSDKTemplate
